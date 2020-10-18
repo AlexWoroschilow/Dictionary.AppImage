e8fcb800f7a27edb027e558c74177558c9751316
AlexWoroschilow <alex.woroschilow@gmail.com>
2020-10-18 13:57:46 +0200

    refactoring

--- src/modules/window_dictionary_popup/__init__.py
+++ src/modules/window_dictionary_popup/__init__.py
@@ -18,7 +18,8 @@ from .gui.dialog import TranslationDialog
 
 
 class Loader(object):
-    popup = None
+    frameless = None
+    framed = None
 
     def __enter__(self):
         return self
@@ -34,6 +35,42 @@ class Loader(object):
         popup = TranslationDialog()
         popup.setText(text)
         popup.show()
+
+        return popup
+
+    @inject.params(config='config')
+    def _popup_frameless(self, text, popup: TranslationDialog = None, config=None):
+        try:
+            self.clear()
+        except RuntimeError as ex:
+            pass
+
+        popup: TranslationDialog = self._popup(text)
+        popup.activated.connect(popup.close)
+        if not int(config.get('popup.position')):
+            return popup
+
+        x = int(config.get('popup.x', 0))
+        y = int(config.get('popup.y', 0))
+        popup.move(x, y)
+        return popup
+
+    @inject.params(config='config')
+    def _popup_framed(self, text, popup: TranslationDialog = None, config=None):
+        try:
+            if not popup: self.clear()
+            if popup: popup.setText(text)
+            if popup: return popup
+        except RuntimeError as ex:
+            self.clear()
+
+        popup: TranslationDialog = self._popup(text)
+        if not int(config.get('popup.position')):
+            return popup
+
+        x = int(config.get('popup.x', 0))
+        y = int(config.get('popup.y', 0))
+        popup.move(QtCore.QPoint(x, y))
 
         return popup
 
@@ -64,37 +101,21 @@ class Loader(object):
         if not translation: return None
 
         if int(config.get('popup.frameless', 1)):
-            try:
-                if self.popup: self.popup.close()
-                if self.popup: self.popup = None
-            except RuntimeError as ex:
-                if self.popup: self.popup = None
+            self.frameless = self._popup_frameless(translation, self.frameless)
+            return self.frameless.exec_()
 
-            self.popup = self._popup(translation)
-            self.popup.activated.connect(self.popup.close)
-            if int(config.get('popup.position')):
-                x = int(config.get('popup.x', 0))
-                y = int(config.get('popup.y', 0))
-                self.popup.move(x, y)
-            return self.popup.exec_()
-
-        if self.popup is not None:
-            try:
-                self.popup.setText(translation)
-                self.popup.finished.connect(self.popup.close)
-                self.popup.finished.connect(self.clear)
-                return self.popup
-            except RuntimeError as ex:
-                pass
-
-        self.popup = self._popup(translation)
-        if int(config.get('popup.position')):
-            x = int(config.get('popup.x', 0))
-            y = int(config.get('popup.y', 0))
-            self.popup.move(QtCore.QPoint(x, y))
-        self.popup.finished.connect(self.popup.close)
-        self.popup.finished.connect(self.clear)
-        return self.popup.show()
+        self.framed = self._popup_framed(translation, self.framed)
+        return self.framed.show()
 
     def clear(self):
-        self.popup = None
+        try:
+            if self.frameless: self.frameless.close()
+            if self.frameless: self.frameless = None
+        except RuntimeError as ex:
+            pass
+
+        try:
+            if self.framed: self.framed.close()
+            if self.framed: self.framed = None
+        except RuntimeError as ex:
+            pass
